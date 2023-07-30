f21ff9b7989075ae5701619db71f099771153abf
Efrain Luciano Fernandez Valverde <efrain19091@gmail.com>
2023-07-30 11:30:56 +0000

    Modal template for creating key moments installed

--- static/js/custom.js
+++ static/js/custom.js
@@ -26,15 +26,58 @@ $(document).ready(function() {
         // Add active class to the selected timeline item
         $('.timeline-item').removeClass('active');
         selectedKeyMoment.addClass('active');
+    });
+});
 
-        // Initialize the resizable and draggable features for the image
-        $('#resizable-image').resizable({
-            aspectRatio: true, // Lock aspect ratio when resizing
-            containment: '#key-moment-image', // Restrict resizing within the frame
-        });
 
-        $('#resizable-image').draggable({
-            containment: '#image-frame', // Restrict dragging within the frame
+$(document).ready(function() {
+    // handle click event on the "Create New Moment" link
+    $('#create-new-moment').on('click', function(event) {
+        event.preventDefault();
+
+        // Open the modal or navigate to the separate page for creating the new moment
+        // You can use Bootstrap modal or any other method to show the form for creating a new moment
+        // For simplicity, let's assume we're using a modal:
+        $('#create-moment-modal').modal('show');
+    });
+});
+
+
+$(document).ready(function() {
+    // handle form submission for creating a new moment
+    $('#new-moment-form').on('submit', function(event) {
+        event.preventDefault();
+
+        // Get the form data
+        var formData = $(this).serialize();
+
+        // Send the form data to the server to create the new moment
+        // You can use AJAX to send the data or submit the form using the traditional form submission
+        // For this example, let's assume we're using AJAX:
+        $.ajax({
+            type: 'POST',
+            url: '{% url "create_key_moment" %}',  // Replace with the actual URL for creating a new moment
+            data: formData,
+            success: function(response) {
+                // On success, update the timeline with the new moment HTML
+                var newMomentHTML = '<div class="timeline-item" data-dates="' + response.start_date + '">'
+                    + '<a href="#">'
+                    + '<h4>' + response.title + '</h4>'
+                    + '<p class="moment-excerpt">' + response.excerpt + '</p>'
+                    + '<p class="moment-description" style="display: none;">' + response.description + '</p>'
+                    + '<img src="' + response.image_url + '" alt="' + response.title + '" style="display: none;">'
+                    + '</a>'
+                    + '</div>';
+
+                $('.history-items').append(newMomentHTML);
+
+                // Close the modal or navigate back to the timeline page
+                $('#create-moment-modal').modal('hide');
+            },
+            error: function(error) {
+                // Handle any errors that may occur during form submission
+            }
         });
     });
 });
+
