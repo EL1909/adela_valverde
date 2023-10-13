2ca13826b8cce1aa4a15f2eda7e540f8a2d74907
Efrain Luciano Fernandez Valverde <efrain19091@gmail.com>
2023-10-13 06:43:09 +0000

    Retrivin information from youtube to website succesfully

--- youtube_posts/urls.py
+++ youtube_posts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('youtube_posts/', views.youtube_posts, name='youtube_posts'),
+    path('youtube_posts/<str:playlist_id>/', views.youtube_playlist_videos, name='youtube_posts'),
     path('simple/', views.simple_view, name='simple_view'),
 ]
--- youtube_posts/views.py
+++ youtube_posts/views.py
@@ -1,56 +1,28 @@
 import os
-from googleapiclient.discovery import build
 from django.shortcuts import render
-from embed_video.fields import EmbedVideoField
-from youtube_data_api import YouTubeDataApi
+from googleapiclient.discovery import build
 from decouple import config
 
 
 def simple_view(request):
     return render(request, 'youtube_posts/simple_template.html')
 
-def video_list(request):
-    youtube_api = YoutubeDataApi()
 
-    # Retrieve a list of all the videos on the Youtube Channel with this ID
-    videos = youtube_api.videos_list(part='snippet', channelId='UCvxBjb0KE26mADhgkECn8rQ')
-
-    # Iterate over the list of videos and display each video on the Template
-    context = {
-        'videos': videos
-    }
-    
-    return render (request, 'youtube_posts/simple_template.html', context)
-
-
-
-def youtube_posts(request):
+def youtube_playlist_videos(request, playlist_id):
     youtube_api_key = config('YOUTUBE_API_KEY')  # Get your API key from settings
     youtube = build('youtube', 'v3', developerKey=youtube_api_key)
 
-    # Replace 'channelId' with your channel's ID or 'mine' to fetch your own videos
-    response = youtube.search().list(
-        channelId='UCvxBjb0KE26mADhgkECn8rQ',
-        type='video',
-        order='date',
-        part='id',
-        maxResults=10  # You can adjust the number of videos to display
+    playlist_items = youtube.playlistItems().list(
+        playlistId=playlist_id,
+        part='snippet',
+        maxResults=10
     ).execute()
 
-    print(response)
-
-    video_ids = [item['id']['videoId'] for item in response['items']]
-    video_details = []
-
-    for video_id in video_ids:
-        video_response = youtube.videos().list(
-            id=video_id,
-            part='snippet'
-        ).execute()
-        video_details.append(video_response['items'][0])
+    video_data = playlist_items['items']
 
     context = {
-        'video_details': video_details,
+        'video_data': video_data,
     }
 
     return render(request, 'youtube_posts/youtube_posts.html', context)
+
