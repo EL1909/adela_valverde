6f7c11fb6f5f7430ea25ea78c5094eaaa3632c47
Efrain Luciano Fernandez Valverde <efrain19091@gmail.com>
2023-10-16 12:36:20 +0000

    Set debug to false

--- adela_valverde/settings.py
+++ adela_valverde/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost','8000-el1909-adelavalverde-n2ux2b2i5rw.ws-eu105.gitpod.io']
 
