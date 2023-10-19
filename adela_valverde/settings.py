798058be0c81167dd0faca442ab2aaf932c0f72c
Efrain Luciano Fernandez Valverde <efrain19091@gmail.com>
2023-10-19 07:42:04 +0000

    remove / from static file url

--- adela_valverde/settings.py
+++ adela_valverde/settings.py
@@ -168,7 +168,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
